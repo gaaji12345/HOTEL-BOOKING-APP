6de1aa59fb7637975e66c1cfe4ca5696e7413939
gaji12345 <gajimax12345@gmail.com>
2025-07-25 12:54:30 +0530

    add new hotel in backend

--- backendnew/src/routes/my-hotels.ts
+++ backendnew/src/routes/my-hotels.ts
@@ -2,6 +2,9 @@ import express,{Request,Response} from "express";
 import multer from'multer';
 const router = express.Router();
 import cloudinary from 'cloudinary'
+import Hotel, {HotelType} from "../models/hotel";
+import verifyToken from "../middleware/auth";
+import {body} from "express-validator";
 
 const storage=multer.memoryStorage();
 
@@ -9,10 +12,27 @@ const upload=multer({storage:storage,
     limits:{fileSize:5 * 1024 * 1024},
 });
 
-router.post('/',upload.array("imageFiles",6 ), async (req:Request, res:Response) =>{
+// @ts-ignore
+router.post('/',verifyToken ,
+    [body("name").notEmpty().withMessage('Name is required'),
+        body("city").notEmpty().withMessage("City is required"),
+        body("country").notEmpty().withMessage("Country is required"),
+        body("description").notEmpty().withMessage("Description is required"),
+        body("type").notEmpty().withMessage("Hotel type is required"),
+        body("pricePerNight")
+            .notEmpty()
+            .isNumeric()
+            .withMessage("Price per night is required and must be a number"),
+        body("facilities")
+            .notEmpty()
+            .isArray()
+            .withMessage("Facilities are required"),
+    ],
+    upload.array("imageFiles",6 ), async (req:Request, res:Response) =>{
     try{
         const imageFiles=req.files as Express.Multer.File[];
-        const newHotel=req.body;
+        const newHotel:HotelType=req.body;
+        
 
         const uploadPromises=imageFiles.map(async (image) => {
             const b64=Buffer.from(image.buffer).toString("base64");
@@ -23,7 +43,17 @@ router.post('/',upload.array("imageFiles",6 ), async (req:Request, res:Response)
 
         const imageUrls=await Promise.all(uploadPromises);
 
+        // @ts-ignore
+        newHotel.imageUrls=imageUrls;
+        newHotel.lastUpdated=new Date();
+        if (req.userId != null) {
+            newHotel.userId = req.userId;
+        }
 
+        const hotel=new Hotel(newHotel);
+        await hotel.save();
+
+        res.status(201).json(hotel);
 
     }catch(e){
         console.log("Error creating hotel",e);
@@ -32,3 +62,5 @@ router.post('/',upload.array("imageFiles",6 ), async (req:Request, res:Response)
     }
 
 })
+
+export default router;
