708f76e6799177b9f70f3598064d925343776276
gaji12345 <gajimax12345@gmail.com>
2025-07-31 16:16:14 +0530

    Fcailites and Image section done

--- frontend/vite-project/src/forms/ManageHotelForm/FacilitiesSection.tsx
+++ frontend/vite-project/src/forms/ManageHotelForm/FacilitiesSection.tsx
@@ -13,7 +13,10 @@ const FacilitiesSection = () => {
             <h2 className="text-2xl font-bold mb-3">Facilities</h2>
             <div className="grid grid-cols-5 gap-3">
                 {hotelFacilities.map((facility) => (
-                    <label className="text-sm flex gap-1 text-gray-700">
+                    <label
+                        key={facility}
+                        className="text-sm flex gap-1 text-gray-700"
+                    >
                         <input
                             type="checkbox"
                             value={facility}
